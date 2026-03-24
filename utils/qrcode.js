8e3038907abac372d417ea26b8d651450e918cd2
AscalonX <153923689+AscalonX@users.noreply.github.com>
2026-03-24 13:12:49 +0700

    fix QR code URL to use frontend URL

--- utils/qrcode.js
+++ utils/qrcode.js
@@ -1,7 +1,7 @@
 const QRCode = require('qrcode');
 
 exports.generateQR = async (data) => {
-    const baseUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT || 5000}`;
-    const url = `${baseUrl}/booking.html?id=${data.reservationId}`;
+    const baseUrl = process.env.FRONTEND_URL || process.env.BASE_URL || `http://localhost:3000`;
+    const url = `${baseUrl}/booking/${data.reservationId}`;
     return await QRCode.toDataURL(url);
 };
